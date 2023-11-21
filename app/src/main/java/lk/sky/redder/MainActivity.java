fa5f4a3efc15c1c96ed062d912f7ea446824d26c
Hashan2kk2 <hashan.lakruwan2020@gmail.com>
2023-11-21 21:12:53 +0530

    first commit

---
+++ app/src/main/java/lk/sky/redder/MainActivity.java
@@ -0,0 +1,25 @@
+package lk.sky.redder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.goto_signin);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signinActivity = new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(signinActivity);
+            }
+        });
+    }
+}
