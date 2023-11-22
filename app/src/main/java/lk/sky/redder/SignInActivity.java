29609eab9730b165105e3bc4df4def0a3173be41
Hashan2kk2 <hashan.lakruwan2020@gmail.com>
2023-11-22 19:27:22 +0530

    Sign in Activity UI Design Completed

--- app/src/main/java/lk/sky/redder/SignInActivity.java
+++ app/src/main/java/lk/sky/redder/SignInActivity.java
@@ -3,6 +3,9 @@ package lk.sky.redder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        TextView terms = findViewById(R.id.termsAndConditions);
+        terms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignInActivity.this, "terms", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
