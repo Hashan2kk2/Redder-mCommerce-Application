1bbd9f151bed28a911118b61afa33a8fe200d42a
Hashan Lakruwan <hashan.lakruwan2020@gmail.com>
2023-11-27 17:56:18 +0530

    Signin and Signup activity UI Design complete

--- app/src/main/java/lk/sky/redder/MainActivity.java
+++ app/src/main/java/lk/sky/redder/MainActivity.java
@@ -1,11 +1,11 @@
 package lk.sky.redder;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn = findViewById(R.id.goto_signin);
+        Button signup = findViewById(R.id.goto_signup);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signinActivity = new Intent(MainActivity.this,SignInActivity.class);
+                Intent signinActivity = new Intent(MainActivity.this, SignInActivity.class);
                 startActivity(signinActivity);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(signup);
             }
         });
     }
